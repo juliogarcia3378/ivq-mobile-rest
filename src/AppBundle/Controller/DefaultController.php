a3dfbe7d636c0ef67560bacee833a4a96f37d31c
Julio Garcia <juliogarciaguevara88@gmail.com>
2016-09-25 09:06:39 -0700

    RestAPI

---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,37 @@
+<?php
+
+
+namespace AppBundle\Controller;
+ 
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Controller\FOSRestController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+ 
+class DefaultController extends FOSRestController
+{
+    /**
+     * @Rest\Get("/hello")
+     * @ApiDoc(
+     *  description="Returns a collection of Object",
+     *  requirements={
+     *      {
+     *          "name"="limit",
+     *          "dataType"="integer",
+     *          "requirement"="\d+",
+     *          "description"="how many objects to return"
+     *      }
+     *  },
+     *  parameters={
+     *      {"name"="categoryId", "dataType"="integer", "required"=true, "description"="category id"}
+     *  }
+     * )
+     */
+    public function indexAction(Request $request)
+    {
+        $view = $this->view(['hello' => 'world'], Response::HTTP_OK);
+        return $view;
+    }
+
+}
